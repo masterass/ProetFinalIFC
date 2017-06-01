0df815ef43ad9c443f98e70a0603bf562a1b65bc
g33k <test@test.test>
2017-06-01 08:56:55 +0200

    Changement clear_screen 2 corriger

--- clear_screen.c
+++ clear_screen.c
@@ -4,15 +4,12 @@ void clear_screen()
 {
     #ifdef __WIN32__
         system("cls");
+
     #else
+        #ifdef __WIN64__
+            system("cls");
+        #else
         system("clear");
+        #endif
     #endif
 }
-
-/**void clean_stdin(void)
-{
-    int c;
-    do {
-        c = getchar();
-    } while (c != '\n' && c != EOF);
-}**/
